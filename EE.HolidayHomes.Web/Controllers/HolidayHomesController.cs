55a4c89e97aa7422c7428310caed4a00845cf191
202370630 <adnane.asabdoun@student.howest.be>
2025-06-10 22:10:10 +0200

    Further implement create method

--- EE.HolidayHomes.Web/Controllers/HolidayHomesController.cs
+++ EE.HolidayHomes.Web/Controllers/HolidayHomesController.cs
@@ -1,5 +1,7 @@
-﻿using EE.HolidayHomes.Web.Data;
+﻿using EE.HolidayHomes.Core.Entities;
+using EE.HolidayHomes.Web.Data;
 using EE.HolidayHomes.Web.ViewModels;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -49,6 +51,65 @@ namespace EE.HolidayHomes.Web.Controllers
 
             };
             return View(holidayHomesCreateViewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(HolidayHomesCreateViewModel holidayHomesCreateViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                holidayHomesCreateViewModel = new HolidayHomesCreateViewModel
+                {
+                    Location = await _applicationDbContext.Locations
+                    .OrderBy(l => l.Name)
+                    .Select(l => new SelectListItem
+                    {
+                        Text = l.Name,
+                        Value = l.Id.ToString()
+                    }).ToListAsync(),
+
+                    HomeType = await _applicationDbContext.HomeTypes
+                            .OrderBy(l => l.Name)
+                            .Select(l => new SelectListItem
+                            {
+                                Text = l.Name,
+                                Value = l.Id.ToString()
+                            }).ToListAsync(),
+
+                    HomeProperties = await _applicationDbContext.HomeProperties
+                            .OrderBy(l => l.Name)
+                            .Select(l => new SelectListItem
+                            {
+                                Text = l.Name,
+                                Value = l.Id.ToString()
+                            }).ToListAsync(),
+
+                };
+                return View(holidayHomesCreateViewModel);
+            }
+            
+            var holidayHome = new HolidayHome();
+            holidayHome.Name = holidayHomesCreateViewModel.Name;
+            holidayHome.Name = holidayHomesCreateViewModel.Name;
+            holidayHome.LocationId = holidayHomesCreateViewModel.LocationId;
+            holidayHome.HomeTypeId = holidayHomesCreateViewModel.HomeTypeId;
+
+            holidayHome.HomeProperties = await _applicationDbContext
+                .HomeProperties
+                .Where(p => holidayHomesCreateViewModel.HomePropertyIds.Contains(p.Id)).ToListAsync();
+
+            
+            await _applicationDbContext.HolidayHomes.AddAsync(holidayHome);
+            try
+            {
+                await _applicationDbContext.SaveChangesAsync();
+            }
+            catch (DbUpdateException dbUpdateException)
+            {
+                Console.WriteLine(dbUpdateException.Message);
+            }
+            return RedirectToAction("Home", "Index");
         }
     }
 }
