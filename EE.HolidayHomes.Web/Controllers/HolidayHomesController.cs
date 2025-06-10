88000a8f6abd862e4bb7d87f0393d3ae4b1c7b89
202370630 <adnane.asabdoun@student.howest.be>
2025-06-10 22:00:45 +0200

    Implement create method

--- EE.HolidayHomes.Web/Controllers/HolidayHomesController.cs
+++ EE.HolidayHomes.Web/Controllers/HolidayHomesController.cs
@@ -1,13 +1,54 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EE.HolidayHomes.Web.Data;
+using EE.HolidayHomes.Web.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace EE.HolidayHomes.Web.Controllers
 {
+
     public class HolidayHomesController : Controller
     {
-        [HttpGet]
-        public IActionResult Create()
+        private readonly ApplicationDbContext _applicationDbContext;
+        private readonly ILogger<HomeController> _logger;
+
+        public HolidayHomesController(ILogger<HomeController> logger, ApplicationDbContext applicationDbContext)
         {
-            return View();
+            _logger = logger;
+            _applicationDbContext = applicationDbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            var holidayHomesCreateViewModel = new HolidayHomesCreateViewModel
+            {
+                Location = await _applicationDbContext.Locations
+                .OrderBy(l => l.Name)
+                .Select(l => new SelectListItem
+                {
+                    Text = l.Name,
+                    Value = l.Id.ToString()
+                }).ToListAsync(),
+
+                HomeType = await _applicationDbContext.HomeTypes
+                .OrderBy(l => l.Name)
+                .Select(l => new SelectListItem
+                {
+                    Text = l.Name,
+                    Value = l.Id.ToString()
+                }).ToListAsync(),
+
+                HomeProperties = await _applicationDbContext.HomeProperties
+                .OrderBy(l => l.Name)
+                .Select(l => new SelectListItem
+                {
+                    Text = l.Name,
+                    Value = l.Id.ToString()
+                }).ToListAsync(),
+
+            };
+            return View(holidayHomesCreateViewModel);
         }
     }
 }
---
+++ EE.HolidayHomes.Web/ViewModels/HolidayHomesCreateViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace EE.HolidayHomes.Web.ViewModels
+{
+    public class HolidayHomesCreateViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+
+        public IEnumerable<SelectListItem> Location { get; set; }
+        [Display(Name = "Locations")]
+        public int LocationId { get; set; }
+
+        public IEnumerable<SelectListItem> HomeType { get; set; }
+        [Display(Name = "HomeTypes")]
+
+        public int HomeTypeId { get; set; }
+
+        public IEnumerable<SelectListItem> HomeProperties { get; set; }
+        [Display(Name = "HomeProperties")]
+        [Required]
+        public IEnumerable<int> HomePropertyIds { get; set; }
+
+    }
+}
